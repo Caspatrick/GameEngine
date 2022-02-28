f902b3353b05e74078b94763ed2c4e3ee58468e8
Patrick Caspari <patrick93c@googlemail.com>
2022-02-28 11:31:06 +0100

    added documentation

--- Project1/GameObject.h
+++ Project1/GameObject.h
@@ -1,7 +1,9 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <string>
-
+/** Class GameObject
+* a class that handles renderable object in the game, such as players, boxes, coins, etc
+**/
 class GameObject {
 	friend class Renderer;
 	sf::Texture texture;
@@ -9,9 +11,12 @@ class GameObject {
 	sf::RectangleShape shape;
 	
 public:
+	//constructor
 	GameObject(std::string texture_file, sf::Vector2f _size, sf::Vector2f _pos);
+	//positioning the object in the window
 	void move(float x, float y);
 	void move(sf::Vector2f dir);
+	//getter function 
 	sf::Vector2f getPos();
 
 };
--- Project1/Renderer.h
+++ Project1/Renderer.h
@@ -3,16 +3,23 @@
 #include "GameObject.h"
 #include <string>
 #include <vector>
+
+/** Class Renderer
+* keeps track of all objects in the window frame and handles what objects are rendered.	
+*/
 class Renderer
 {
 	sf::RenderWindow *window;
 	std::vector<GameObject*> renderObjects;
 
 public:
+	//constructor
 	Renderer(int sizeX, int sizeY, std::string title);
 	Renderer(sf::RenderWindow* _window);
 
+	//add GameObject to be rendererd on the window
 	void add(GameObject* obj);
+	//render all objects
 	void draw();
 };
 
